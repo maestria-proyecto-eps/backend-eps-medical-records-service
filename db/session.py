2d6ef29c9c852d013ed8492d0a6efc85e6fb8a98
Juan David <jdroserot@udistrital.edu.co>
2026-03-15 20:22:47 -0500

    update db connection way

--- db/session.py
+++ db/session.py
@@ -1,6 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import NullPool, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
+
+USER = settings.DB_ADMIN_USER
+PASSWORD = settings.DB_ADMIN_PASSWORD
+HOST = settings.DB_ADMIN_HOST
+PORT = settings.DB_ADMIN_PORT
+DBNAME = settings.DB_ADMIN_NAME
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 # Crear engine (sincrónico)
 engine = create_engine(
