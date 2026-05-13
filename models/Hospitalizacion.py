308bdae8e4ccdb5ef8ae89d4ff430f4b5066b0cf
gyuheeJo <davidgyuhee@gmail.com>
2026-05-13 12:35:27 -0500

    Model changed

--- models/Hospitalizacion.py
+++ models/Hospitalizacion.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, SmallInteger
 
 from db.session import Base
 
@@ -11,6 +11,7 @@ class Hospitalizacion(Base):
     num_cama = Column(Integer, nullable=False)
     ingreso = Column(DateTime, nullable=False)
     salida = Column(DateTime, nullable=True)
+    estado = Column(SmallInteger, nullable=False)
 
     def __repr__(self):
         return f"Hospitalizacion(id_hospitalizacion={self.id_hospitalizacion}, id_urgencia={self.id_urgencia}, num_cama={self.num_cama})"
