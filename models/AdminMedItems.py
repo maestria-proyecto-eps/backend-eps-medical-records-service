eb95afdecd570a138279da7d55473a350b54bec4
gyuheeJo <davidgyuhee@gmail.com>
2026-05-26 12:41:57 -0500

    error fixed

--- models/AdminMedItems.py
+++ models/AdminMedItems.py
@@ -3,9 +3,8 @@ from db.session import Base
 
 
 class AdminMedItems(Base):
-    __tablename__ = "ADMINISTRACIONMED_ITEMS"
+    __tablename__ = "administracion_med_items"
 
     id_admin_items = Column(Integer, primary_key=True)
-    id_prescripcion_items =  Column(Integer)
+    id_prescripcion_item =  Column(Integer)
     id_admin_med = Column(Integer, ForeignKey("administracion_medicamentos.id_admin_med"),nullable=False)
-    id_items = Column(Integer, ForeignKey("prescripciones_items.id_items"), nullable=False)
